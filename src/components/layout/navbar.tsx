5ca1dd631694899f9bb8c5eeee7d92d004fccebf
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-03-23 17:11:52 +1100

    feat: add basic navbar layout component

---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,13 @@
+import { Button } from '../ui/button'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Button variant="nav">About</Button>
+      <Button variant="nav">Projects</Button>
+      <Button variant="nav">Experience</Button>
+      <Button variant="nav">Skills</Button>
+      <Button variant="nav">Contact</Button>
+    </nav>
+  )
+}
