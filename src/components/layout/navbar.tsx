0d639592b9ec0a952c2a24984c4016a48b32d4cc
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-03-24 11:48:02 +1100

    feat: add navbar styling and mapping

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,13 +1,26 @@
 import { Button } from '../ui/button'
 
+const navbarItems = [
+  { href: '#home', name: 'Home' },
+  { href: '#projects', name: 'Projects' },
+  { href: '#experience', name: 'Experience' },
+  { href: '#skills', name: 'Skills' },
+  { href: '#contact', name: 'Contact' },
+]
+
 export default function Navbar() {
   return (
     <nav>
-      <Button variant="nav">About</Button>
-      <Button variant="nav">Projects</Button>
-      <Button variant="nav">Experience</Button>
-      <Button variant="nav">Skills</Button>
-      <Button variant="nav">Contact</Button>
+      {navbarItems.map((link) => (
+        <Button key={link.href} asChild variant="nav">
+          <a
+            className="text-foreground no-underline hover:text-catppuccin-macchiato-mauve"
+            href={link.href}
+          >
+            {link.name}
+          </a>
+        </Button>
+      ))}
     </nav>
   )
 }
