5e324c1eb316896a12cbc7435bf76ed877e08c1f
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-03-25 10:38:55 +1100

    feat: adjust styling for header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,10 +3,10 @@ import Navbar from './navbar'
 export default function Header() {
   return (
     <header className="mt-4 flex justify-between">
-      <h1 className="text-[#b8c0e0] mt-1.25">
-        <span className="text-sky-300">{`{ `}</span>
+      <h1 className="text-term-text mt-1.25">
+        <span className="text-term-green">{`{ `}</span>
         {`riv.dev`}
-        <span className="text-sky-300">{` }`}</span>
+        <span className="text-term-green">{` }`}</span>
       </h1>
 
       <Navbar />
