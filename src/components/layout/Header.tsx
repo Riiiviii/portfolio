5caf1a1a5908dc1f3dfd3c0ce45308723d7a8a24
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-03-25 10:49:22 +1100

    feat: adjust colour colour palette

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,11 +2,11 @@ import Navbar from './navbar'
 
 export default function Header() {
   return (
-    <header className="mt-4 flex justify-between">
+    <header className="mt-6 flex justify-between">
       <h1 className="text-term-text mt-1.25">
-        <span className="text-term-green">{`{ `}</span>
+        <span className="text-term-white">{`{ `}</span>
         {`riv.dev`}
-        <span className="text-term-green">{` }`}</span>
+        <span className="text-term-white">{` }`}</span>
       </h1>
 
       <Navbar />
--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,4 +1,4 @@
-import { Mail } from 'lucide-react'
+import { Download, Github, Linkedin, Mail } from 'lucide-react'
 import { Button } from '../ui/button'
 
 export default function Hero() {
@@ -8,21 +8,40 @@ export default function Hero() {
         <div className="flex max-w-2xl flex-col items-start text-left">
           <h1 className="text-term-text text-5xl font-bold">
             {"Hi! I'm "}
-            <span className="text-term-green">Gabriel Riven Wahnich</span>
+            <span className="text-term-white">Gabriel Riven Wahnich</span>
           </h1>
           <p className="mt-4 text-l text-term-text-dim leading-relaxed">
-            {"I'm a Melbourne-based software developer with experience building and delivering "}
-            <span className="text-term-green">real client applications</span>
-            {", from simple frontend interfaces to "}
-            <span className="text-term-green">full-stack systems</span>
-            {" with authentication and database integration. I focus on "}
-            <span className="text-term-green">backend development</span>
-            {"—designing APIs, handling data, and writing clear, maintainable code for real-world use."}
+            {
+              "I'm a Melbourne-based software developer with experience building and delivering "
+            }
+            <span className="text-term-white">real client applications</span>
+            {', from simple frontend interfaces to '}
+            <span className="text-term-white">full-stack systems</span>
+            {' with authentication and database integration. I focus on '}
+            <span className="text-term-white">backend development</span>
+            {
+              '—designing APIs, handling data, and writing clear, maintainable code for real-world use.'
+            }
           </p>
           <div className="mt-10 flex flex-wrap gap-3">
-            <Button variant="primary">
+            <Button variant="glitch">
               <Mail />
               Contact
+            </Button>
+            <span className="mt-2">/</span>
+            <Button variant="glitch">
+              <Download />
+              Resume
+            </Button>
+            <span className="mt-2">/</span>
+            <Button variant="glitch">
+              <Github />
+              Github
+            </Button>
+            <span className="mt-2">/</span>
+            <Button variant="glitch">
+              <Linkedin />
+              Linkdin
             </Button>
           </div>
         </div>
