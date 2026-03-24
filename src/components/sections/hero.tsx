f0a9495541819f54945c894f151627f62399dc1d
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-03-24 12:21:38 +1100

    feat: add contact button

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,9 +1,12 @@
+import { Mail } from 'lucide-react'
+import { Button } from '../ui/button'
+
 export default function Hero() {
   return (
     <section id="home" className="mt-30">
       <div className="grid items-center gap-10 md:grid-cols-[3fr_2fr]">
         <div className="flex max-w-2xl flex-col items-start text-left">
-          <h1 className="text-[#b8c0e0] text-4xl font-bold">
+          <h1 className="text-[#b8c0e0] text-5xl font-bold">
             Hi! I'm <span className="text-sky-300">Gabriel Riven Wahnich</span>
           </h1>
           <p className="mt-4 text-l text-[#a5adcb] leading-relaxed">
@@ -17,6 +20,12 @@ export default function Hero() {
             —designing APIs, handling data, and writing clear, maintainable code
             for real-world use.
           </p>
+          <div className="mt-10 flex flex-wrap gap-3">
+            <Button variant="heroPrimary">
+              <Mail />
+              Contact
+            </Button>
+          </div>
         </div>
       </div>
     </section>
