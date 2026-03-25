b7472da5de5648ba3171cc30aaf4384801be723b
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-03-25 13:47:07 +1100

    feat: create skills section component

---
+++ src/components/sections/skills.tsx
@@ -0,0 +1,11 @@
+export default function Skills() {
+  return (
+    <section id="skills" className="mt-40">
+      <img
+        src="skills.png"
+        alt="skills title image"
+        className="invert mix-blend-screen"
+      />
+    </section>
+  )
+}
