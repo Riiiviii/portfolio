5bc74db321d30e580938ef20129f09725a9ac4ce
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-05-24 12:09:52 +1000

    feat: add chat message component

---
+++ src/components/chat/chat-message.tsx
@@ -0,0 +1,46 @@
+import type { ComponentProps } from "react"
+import { twMerge } from "tailwind-merge"
+
+type ChatMessageProps = {
+    role: Role
+} & ComponentProps<"div">
+
+function ChatMessage({ children, role, ...props }: ChatMessageProps) {
+    return (
+        <div className="max-w-[85%]" {...props}>
+            <div
+                className={twMerge(
+                    "text-xs text-muted-foreground px-3 py-1",
+                    role === "user" ? "text-right" : "text-left"
+                )}
+            >
+                {role === "user" ? "YOU" : "riven-ai"}
+            </div>
+            <p
+                className={twMerge(
+                    "text-sm py-1 px-3",
+                    variantStyle(role)
+                )}
+            >
+                {role === "assistant" && (
+                    <span className="text-xs font-bold">{">"} </span>
+                )}
+                {children}
+            </p>
+        </div>
+    )
+}
+
+export default ChatMessage
+
+function variantStyle(role: Role) {
+    switch (role) {
+        case "user":
+            return "border rounded-tr rounded-tl rounded-bl text-sm border-white"
+        case "assistant":
+            return ""
+        default:
+            throw new Error(`Invalid role: ${role satisfies never}`)
+    }
+}
+
