aad8287d5c202fe3f3e146c8b7d70f8ea020fa8f
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-05-25 15:54:05 +1000

    fix: add required imports to message component

--- src/components/chat/chat-message.tsx
+++ src/components/chat/chat-message.tsx
@@ -1,46 +1,41 @@
-import type { ComponentProps } from "react"
-import { twMerge } from "tailwind-merge"
+import type { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+import type { Role } from "./types";
 
 type ChatMessageProps = {
-    role: Role
-} & ComponentProps<"div">
+	role: Role;
+} & ComponentProps<"div">;
 
 function ChatMessage({ children, role, ...props }: ChatMessageProps) {
-    return (
-        <div className="max-w-[85%]" {...props}>
-            <div
-                className={twMerge(
-                    "text-xs text-muted-foreground px-3 py-1",
-                    role === "user" ? "text-right" : "text-left"
-                )}
-            >
-                {role === "user" ? "YOU" : "riven-ai"}
-            </div>
-            <p
-                className={twMerge(
-                    "text-sm py-1 px-3",
-                    variantStyle(role)
-                )}
-            >
-                {role === "assistant" && (
-                    <span className="text-xs font-bold">{">"} </span>
-                )}
-                {children}
-            </p>
-        </div>
-    )
+	return (
+		<div className="max-w-[85%]" {...props}>
+			<div
+				className={twMerge(
+					"text-xs text-muted-foreground px-3 py-1",
+					role === "user" ? "text-right" : "text-left",
+				)}
+			>
+				{role === "user" ? "YOU" : "riven-ai"}
+			</div>
+			<p className={twMerge("text-sm py-1 px-3", variantStyle(role))}>
+				{role === "assistant" && (
+					<span className="text-xs font-bold">{">"} </span>
+				)}
+				{children}
+			</p>
+		</div>
+	);
 }
 
-export default ChatMessage
+export default ChatMessage;
 
 function variantStyle(role: Role) {
-    switch (role) {
-        case "user":
-            return "border rounded-tr rounded-tl rounded-bl text-sm border-white"
-        case "assistant":
-            return ""
-        default:
-            throw new Error(`Invalid role: ${role satisfies never}`)
-    }
+	switch (role) {
+		case "user":
+			return "border rounded-tr rounded-tl rounded-bl text-sm border-white";
+		case "assistant":
+			return "";
+		default:
+			throw new Error(`Invalid role: ${role satisfies never}`);
+	}
 }
-
