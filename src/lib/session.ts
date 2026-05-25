9c935b0da25b39973156b7293facca0a1be0db94
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-05-25 17:19:03 +1000

    fix: getting session pre-render causing errors

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,10 +1,14 @@
 import { v4 as uuid } from "uuid";
 
 export function getSessionId(): string {
-	let sessionId = localStorage.getItem("session_id");
+	if (typeof window === "undefined") {
+		return "";
+	}
+
+	let sessionId = window.localStorage.getItem("session_id");
 	if (sessionId === null) {
 		sessionId = uuid();
-		localStorage.setItem("session_id", sessionId);
+		window.localStorage.setItem("session_id", sessionId);
 	}
 	return sessionId;
 }
